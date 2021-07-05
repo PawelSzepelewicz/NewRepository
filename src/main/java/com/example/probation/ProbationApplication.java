9eaa2dd559ba6945776ed431a5ebaebf79040ef2
Pawel Szepelewicz <pashashepel1997@outlook.com>
2021-07-05 22:22:54 +0300

    First implementation

---
+++ src/main/java/com/example/probation/ProbationApplication.java
@@ -0,0 +1,15 @@
+package com.example.probation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class ProbationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbationApplication.class, args);
+	}
+
+}
