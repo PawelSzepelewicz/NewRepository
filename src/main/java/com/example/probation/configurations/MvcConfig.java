123175577a4934e4172ebf32333b90f918c03575
Pawel Szepelewicz <pashashepel1997@outlook.com>
2021-08-03 20:53:03 +0300

    Bug fixes

--- src/main/java/com/example/probation/configurations/MvcConfig.java
+++ src/main/java/com/example/probation/configurations/MvcConfig.java
@@ -1,12 +1,9 @@
 package com.example.probation.configurations;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
@@ -14,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
-import java.util.Properties;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
